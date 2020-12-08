d8282b1054ce4551651b77029aeadc7711b79b69
Pham Van Hieu <mr_hie@yahoo.com>
2020-12-09 00:07:27 +0800

    wip: update HeroSlide

--- src/components/HeroSlide/HeroSlide.js
+++ src/components/HeroSlide/HeroSlide.js
@@ -25,9 +25,9 @@ export default () => (
 
         <div className="col-md-5">
           <div className="slide-images">
-            <img src="/images/export/c338dfea681155d680a3bcf23d12c0a507873b57-828x1792.jpg" alt="" className="image-item"/>
-            <img src="/images/export/bab21962864371a0c15447b5bbdf3abeff0f5bf5-828x1792.jpg" alt="" className="image-item" />
-            <img src="/images/export/8c4d0c24104440c2ef820abffd587dc3f57f2c80-828x1792.jpg" alt="" className="image-item" />
+            <img src="/images/import/c338dfea681155d680a3bcf23d12c0a507873b57-828x1792.jpg" alt="" className="image-item"/>
+            <img src="/images/import/bab21962864371a0c15447b5bbdf3abeff0f5bf5-828x1792.jpg" alt="" className="image-item" />
+            <img src="/images/import/8c4d0c24104440c2ef820abffd587dc3f57f2c80-828x1792.jpg" alt="" className="image-item" />
           </div>
         </div>
       </div>
