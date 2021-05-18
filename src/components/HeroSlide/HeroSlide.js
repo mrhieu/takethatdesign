dd5f4bb37ebb8b9be14fd643fc99eb107d06092e
Pham Van Hieu <mr_hie@yahoo.com>
2021-05-19 00:52:15 +0800

    feat: add Tinder Vue

--- src/components/HeroSlide/HeroSlide.js
+++ src/components/HeroSlide/HeroSlide.js
@@ -11,12 +11,12 @@ export default () => (
             <div>
               <div className="label-latest">Latest</div>
               <div className="slide-title">
-                Tinder Ionic React 5
+                Tinder Ionic Vue
               </div>
               <div className="slide-subtitle">
-                Tinder UI is <strong>finally</strong> available in Ionic React 5!
+                Tinder UI is <strong>finally</strong> available in Ionic Vue (Vue 3, Ionic 5)
               </div>
-              <Link to="/theme/tinder-react-5/" className="slide-cta">
+              <Link to="/theme/tinder-vue-5/" className="slide-cta">
                 Take a look
               </Link>
             </div>
@@ -25,9 +25,9 @@ export default () => (
 
         <div className="col-md-5">
           <div className="slide-images">
-            <img src="/images/import/c338dfea681155d680a3bcf23d12c0a507873b57-828x1792.jpg" alt="" className="image-item"/>
-            <img src="/images/import/bab21962864371a0c15447b5bbdf3abeff0f5bf5-828x1792.jpg" alt="" className="image-item" />
-            <img src="/images/import/8c4d0c24104440c2ef820abffd587dc3f57f2c80-828x1792.jpg" alt="" className="image-item" />
+            <img src="https://cdn.sanity.io/images/o607ve1z/leafdb/69e9f77a530a23933f44af0a435ff9065694281e-828x1792.png?w=600&h=1299&fit=crop" alt="" className="image-item"/>
+            <img src="https://cdn.sanity.io/images/o607ve1z/leafdb/702f933c560210df77808d7e3f1003553c2beb68-828x1792.png?w=600&h=1299&fit=crop" alt="" className="image-item" />
+            <img src="https://cdn.sanity.io/images/o607ve1z/leafdb/098d367392c2673d38f6f514565e0b1399b4a2f6-828x1792.png?w=600&h=1299&fit=crop" alt="" className="image-item" />
           </div>
         </div>
       </div>
