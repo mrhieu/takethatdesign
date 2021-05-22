c6326916633e83ccb0641669353a0f03cd695b57
Pham Van Hieu <mr_hie@yahoo.com>
2021-05-22 23:20:11 +0800

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-111c6bed2cb8d7217a04.js"
+    "url": "webpack-runtime-90b91f0c6e07b1c7e7d7.js"
   },
   {
     "url": "styles.a8cd51e15478d7ba8077.css"
@@ -46,7 +46,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "8cf05be491ad00e663b1834ae4422bf8"
+    "revision": "c577dbbb64f562be25ada8e5cb9bfe4d"
   },
   {
     "url": "manifest.webmanifest",
