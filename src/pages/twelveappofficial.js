10367823e88879d725308f01e91712325398296a
Pham Van Hieu <mr_hie@yahoo.com>
2021-06-16 22:15:51 +0800

    feat: add app store link

--- src/pages/twelveappofficial.js
+++ src/pages/twelveappofficial.js
@@ -5,7 +5,9 @@ const styles = {
   thumbnail: {
     width: '80%',
     maxWidth: 600,
-  }
+    boxShadow: '0 4px 16px rgba(0, 0, 0, 0.1)',
+    borderRadius: 4,
+  },
 }
 
 export default ({ location }) => (
@@ -18,10 +20,13 @@ export default ({ location }) => (
     <div className="row">
       <article className="col-lg-8 offset-lg-2 text-center">
         <div className="post-content">
-          <h2 id="why">Welcome to Twelve O'clock App Homepage</h2>
+          <h2 id="why">Welcome to <br/> Twelve O'clock App</h2>
           <p className="post-body-text">
-            Download Link: Coming soon
+            The easiest way to track your watch accuracy
           </p>
+          <a href="https://apps.apple.com/vn/app/twelve-oclock/id1570977980" className="inline m-b">
+            <img src="/images/download_appstore.png" height="50" alt=""/>
+          </a>
           <img src="/images/twelve/thumbnail.png" style={ styles.thumbnail } alt=""/>
         </div>
       </article>
